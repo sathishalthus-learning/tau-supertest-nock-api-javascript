b4a67584f26ebadacd242400d87bb67f98917529
sathishalthus-learning <sathishalthus.learning@gmail.com>
2024-10-25 21:01:46 +0530

    First Test

--- tests/firstTest.spec.js
+++ tests/firstTest.spec.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const request = require('supertest');
-const expect = require('chai').expect;
+import express from 'express';
+import request from 'supertest';
+import { expect } from 'chai';
 
-// const app = express();
+const app = express();
 
-// app.get('/first',(err,res)=> {
-//     res.status(200).json({"ok":"response"});
-// });
+app.get('/first',(err,res)=> {
+    res.status(200).json({"ok":"response"});
+});
 
-// describe("first test",()=> {
-//     it('ok response',()=>{
-//         request(app)
-//         .get('/first')
-//         .end((err,res)=>{
-//                 expect(res.statusCode).to.be.equal(200);
-//         });
-//     });
-// })
+describe("first test",()=> {
+    it('ok response',()=>{
+        request(app)
+        .get('/first')
+        .end((err,res)=>{
+                expect(res.statusCode).to.be.equal(200);
+        });
+    });
+})
 
-describe("mocky.io",()=>{
+describe("mocky.io",(done)=>{
     it('mocky check',()=>{
         request('http://www.mocky.io')
         .get('v2/')
