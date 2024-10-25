16145cb0ae9b58c4bdaef6824a18006a5248f178
sathishalthus-learning <sathishalthus.learning@gmail.com>
2024-10-25 23:52:47 +0530

    Response Test

--- tests/response.spec.js
+++ tests/response.spec.js
@@ -1,3 +1,31 @@
 import app from "../src/app.js";
 import request from "supertest";
 import { expect } from "chai";
+
+describe('response',()=>{
+    it('json response',()=>{
+        request(app)
+        .get('/course')
+        .end((err,res)=>{
+            expect(res.body.id).to.be.equal('1');
+        });
+    });
+
+    //
+    it('text response',()=>{
+        request(app)
+        .get('/course')
+        .end((err,res)=>{
+            expect(res.text).to.contain('1');
+        });
+    });
+
+    //
+    it('status response',()=>{
+        request(app)
+        .get('/course')
+        .end((err,res)=>{
+            expect(res.ok).to.be.true;
+        });
+    });
+});
